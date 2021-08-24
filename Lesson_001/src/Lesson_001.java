e6c7e7b89934654a75218bafac9cd047195e0365
Tomomichi Sato <tsato@dcworks.jp>
2021-08-24 22:45:36 +0900

    ベース作成。

---
+++ Lesson_001/src/Lesson_001.java
@@ -0,0 +1,15 @@
+
+/**
+ * Lesson 001.
+ * <p>
+ * コンソールに「hello world」を表示するコマンドラインアプリを作成する。
+ * </p>
+ *
+ * @author tomo-sato
+ */
+public class Lesson_001 {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
